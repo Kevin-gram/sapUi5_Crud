fd668f33959deb716595f43e3bade22055837c8b
Kevin Nyiringango <k.nyiringan.alustudent.com>
2024-12-05 08:15:45 +0200

    adding changes

--- webapp/test/specs/sample.e2e.js
+++ webapp/test/specs/sample.e2e.js
@@ -3,18 +3,13 @@ const { expect } = require('expect-webdriverio');
 describe('My SAPUI5 application', () => {
     it('should have the right title', () => {
         browser.url('/'); 
-        const currentUrl = browser.getUrl();
-        console.log('Navigated to URL:', currentUrl); 
         const title = browser.getTitle();
-        console.log('Page title:', title); 
         expect(title).toBe('SAPUI5 App'); 
     });
 
     it('should open the detail column when a product is clicked', () => {
         browser.url('/'); 
-        const currentUrl = browser.getUrl();
-        console.log('Navigated to URL:', currentUrl); 
-        const product = $('selector-for-product'); 
+        const product = $('selector-for-product');
         product.click();
         const detailColumn = $('selector-for-detail-column'); 
         expect(detailColumn).toBeDisplayed();
