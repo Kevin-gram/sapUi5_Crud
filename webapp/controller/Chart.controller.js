ba4021c7432e43e265707f2dd2754d97aac7a382
Kevin-gram <k.nyiringan@alustudent.com>
2024-12-12 11:20:29 +0200

    detailed pie chart

--- webapp/controller/Chart.controller.js
+++ webapp/controller/Chart.controller.js
@@ -1,22 +1,67 @@
 sap.ui.define([
     "./BaseController",
-    "sap/ui/model/json/JSONModel"
-], function (BaseController, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/odata/v2/ODataModel"
+], function (BaseController, JSONModel, ODataModel) {
     "use strict";
 
     return BaseController.extend("crud.controller.Chart", {
         onInit: function () {
-            // Sample data for the pie chart
-            var oData = {
-                RatingDistribution: [
-                    { Category: "High", Count: 10 },
-                    { Category: "Medium", Count: 20 },
-                    { Category: "Low", Count: 5 }
-                ]
-            };
+            this._loadChartData();
+        },
 
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
+        _loadChartData: function () {
+            let oModel = new ODataModel("http://localhost:3000/odata", {
+                defaultBindingMode: "TwoWay",
+                useBatch: false,
+                headers: {
+                    "Content-Type": "application/atom+xml",
+                },
+                json: false,
+                maxDataServiceVersion: "3.0"
+            });
+
+            oModel.read("/Products", {
+                success: (data) => {
+                    console.log(data);
+                    let ratingDistribution = {
+                        RatingDistribution: [
+                            { Category: "High", Count: 0, Products: [] },
+                            { Category: "Medium", Count: 0, Products: [] },
+                            { Category: "Low", Count: 0, Products: [] }
+                        ]
+                    };
+
+                    data.results.forEach(function (product) {
+                        if (product.Rating >= 4) {
+                            ratingDistribution.RatingDistribution[0].Count++;
+                            ratingDistribution.RatingDistribution[0].Products.push({ ID: product.ID, Name: product.Name });
+                        } else if (product.Rating >= 2) {
+                            ratingDistribution.RatingDistribution[1].Count++;
+                            ratingDistribution.RatingDistribution[1].Products.push({ ID: product.ID, Name: product.Name });
+                        } else {
+                            ratingDistribution.RatingDistribution[2].Count++;
+                            ratingDistribution.RatingDistribution[2].Products.push({ ID: product.ID, Name: product.Name });
+                        }
+                    });
+
+                    let oRatingModel = new JSONModel(ratingDistribution);
+                    this.getView().setModel(oRatingModel);
+                },
+                error: (error) => {
+                    console.error("Error fetching products:", error);
+                }
+            });
+        },
+
+        onProductPress: function (oEvent) {
+            const oItem = oEvent.getSource();
+            const oBindingContext = oItem.getBindingContext();
+            const sPath = oBindingContext.getPath();
+            const oProduct = oBindingContext.getModel().getProperty(sPath);
+
+            const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("detail", { productId: oProduct.ID });
         }
     });
 });
