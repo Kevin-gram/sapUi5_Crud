1f8ed18198dc440b91e379fc8b9385e5423c0a7d
Kevin-gram <k.nyiringan@alustudent.com>
2024-12-13 12:11:58 +0200

    create person controller

--- webapp/controller/Persons.controller.js
+++ webapp/controller/Persons.controller.js
@@ -1,18 +1,14 @@
-sap.ui.define([
-    "./BaseController",
-    "sap/ui/model/odata/v2/ODataModel"
-], function (BaseController,ODataModel) {
+sap.ui.define(["./BaseController", "sap/ui/model/odata/v2/ODataModel"], function (BaseController, ODataModel) {
     "use strict";
-
     return BaseController.extend("crud.controller.Persons", {
         onInit: function () {
-            // Ensure model is set up correctly
-            let oModel = new ODataModel("http://localhost:3000/odata",{
-                defaultBindingMode:"TwoWay",
-                maxDataServiceVersion:"3.0"
-            })
-           this.getView().setModel(oModel)
-        },
-
-    })   
+            // Correctly declare the variable and initialize the OData model
+            let oModel = new ODataModel("http://localhost:3000/odata", {
+                defaultBindingMode: "TwoWay", // Corrected property name
+                maxDataServiceVersion: "3.0"
+            });
+            // Correctly set the model to the view
+            this.getView().setModel(oModel); // Corrected method name
+        }
+    });
 });
